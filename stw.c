092f71cc61238d8e592823ce303d21ca17edb423
sineemore <sineemore@disroot.org>
2020-01-20 16:03:59 +0300

    removed unused includes

--- stw.c
+++ stw.c
@@ -1,24 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 #include <math.h>
-#include <errno.h>
-#include <locale.h>
 #include <poll.h>
-#include <signal.h>
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <X11/cursorfont.h>
-#include <X11/keysym.h>
-#include <X11/Xatom.h>
 #include <X11/Xft/Xft.h>
 #include <X11/Xlib.h>
-#include <X11/Xproto.h>
-#include <X11/Xutil.h>
 
 #include "arg.h"
 #include "drw.h"
