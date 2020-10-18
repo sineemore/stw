1b97e71931c239ad1e08eca0a56de6ccde5d8d35
sineemore <sineemore@disroot.org>
2020-10-18 13:51:18 +0300

    default values for xyXY should be 0
    By default the window must appear in the top left screen corner.

--- config.h
+++ config.h
@@ -6,16 +6,11 @@ static char font[] = "monospace:size=10";
 /* background opacity */
 static double alpha = 1.0;
 
-/* X window geometry
-px, py values position window top left corner in the center of the screen,
-after that tx, ty values adjust window position.
-percentages in tx and ty reference window width and height,
-similarly to CSS translate function
-*/
-struct g px = {.value = 50, .suffix = '%'};
-struct g py = {.value = 50, .suffix = '%'};
-struct g tx = {.prefix = '-', .value = 50, .suffix = '%'};
-struct g ty = {.prefix = '-', .value = 50, .suffix = '%'};
+/* X window geometry */
+struct g px = {0};
+struct g py = {0};
+struct g tx = {0};
+struct g ty = {0};
 
 /* text alignment: l, r and c for left, right and centered respectively */
 static char align = 'l';
