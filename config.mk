497d886541cba41629c74ed5e67896a8a61f2648
planet36 <planet36@users.noreply.github.com>
2022-07-10 08:53:32 -0500

    Remove -Werror
    Add -Wno-shadow -Wno-switch-default

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # See LICENSE file for copyright and license details. 
 
 # paths
-PREFIX    = /usr/local
+PREFIX ?= /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 # includes and libs
@@ -10,5 +10,5 @@ LIBS = -lX11 -lXft -lXrender `pkg-config --libs fontconfig`
 
 # flags
 STWCPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2
-STWCFLAGS   = -std=c99 -pedantic -Wall -Werror $(INCS) $(STWCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STWCFLAGS   = -std=c99 -pedantic -Wall -Wno-shadow -Wno-switch-default $(INCS) $(STWCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STWLDFLAGS  = $(LIBS) $(LDFLAGS)
