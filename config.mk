e1213ffaf504db7838a31d3c008c4a9078933251
planet36 <planet36@users.noreply.github.com>
2022-08-08 20:18:37 -0500

    Add VERSION variable
    Use `pkg-config` to automatically get "-I" and "-l" values
    Move $(INCS) to STWCPPFLAGS

--- config.mk
+++ config.mk
@@ -1,14 +1,17 @@
-# See LICENSE file for copyright and license details. 
+# stw version
+VERSION := 0.4
+
+# Customize below to fit your system
 
 # paths
 PREFIX ?= /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 # includes and libs
-INCS = -I/usr/include/X11 `pkg-config --cflags fontconfig`
-LIBS = -lX11 -lXft -lXrender `pkg-config --libs fontconfig`
+INCS = `pkg-config --cflags fontconfig`
+LIBS = `pkg-config --libs fontconfig xft xrender`
 
 # flags
-STWCPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2
-STWCFLAGS   = -std=c99 -pedantic -Wall -Wno-shadow -Wno-switch-default $(INCS) $(STWCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STWCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 $(INCS)
+STWCFLAGS   = -std=c99 -pedantic -Wall -Wno-shadow -Wno-sign-compare $(STWCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STWLDFLAGS  = $(LIBS) $(LDFLAGS)
