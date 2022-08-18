f89f90d219fe15756deb50d1a44d5b02687b51bd
planet36 <planet36@users.noreply.github.com>
2022-08-18 06:46:27 -0500

    Remove extra space in usage message

--- stw.c
+++ stw.c
@@ -77,7 +77,7 @@ static void
 usage()
 {
 	die(
-"usage: stw [-Vt ] [-x pos] [-y pos] [-X pos] [-Y pos] [-a align]\n"
+"usage: stw [-Vt] [-x pos] [-y pos] [-X pos] [-Y pos] [-a align]\n"
 "           [-f foreground] [-b background] [-F font] [-B borderpx]\n"
 "           [-p period] [-A alpha] command [arg ...]"
 	);
